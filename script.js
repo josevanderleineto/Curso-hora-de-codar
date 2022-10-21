20f947efb510ec815b4c39b8596c6571106290c0
Vanderlei Neto <110213977+josevanderleineto@users.noreply.github.com>
2022-10-21 16:32:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log('Hellho World JS')
+
+//cosole
+
+console.error('Erro');
+console.warn('Aviso');
+console.log('Esta mensagem');
+
+// variaveis
+
+var x = 10;
+//Mainiras atuais
+
+let y = 15
+const z = 20
+
+if(1) {
+  let y = 55;
+  console.log(y);
+}
+
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+y = 25
+
+console.log(y);
+
+//tipos de dados
+const name = 'Vandelei';
+console.log(name);
+console.log(typeof name);
+
+const shirtsQty = 4;
+console.log(shirtsQty);
+console.log(typeof sirtseQty);
+
+const decimal = 12.4;
+console.log(decimal);
+console.log(typeof decimal);
